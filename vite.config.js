6d58ec1adc42867dfe980e385ace2513ea4726b1
unknown <harshithrajf@gmail.com>
2025-09-28 17:22:03 +0530

    Fix: Correct vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+// THIS IS THE CORRECT CODE
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' // Make sure this is imported
-import autoprefixer from 'autoprefixer' // Make sure this is imported
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer], // Make sure this line is here
+      plugins: [tailwindcss, autoprefixer],
     },
   },
   plugins: [react()],
